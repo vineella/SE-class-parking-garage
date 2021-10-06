650a7635ddfcb2888d3de318ce6184b5b0845539
vineella <91093619+vineella@users.noreply.github.com>
2021-10-06 12:29:22 -0400

    Add files via upload
    added way to check what spots are available specifically and for the whole parking garage. Added the parking garage array that will be used to search what spots are avaiable

--- parkingGarage.java
+++ parkingGarage.java
@@ -1,42 +1,63 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 class parkingGarage {
-    int spots, floor;
+    int spots, floors, garage[][];
+    int userFloor, userSpots;
 
     //constructor declaring class
-    public parkingGarage(int floor, int spots){
-        this.floor = floor;
+    public parkingGarage(int floors, int spots){
+        this.floors = floors;
         this.spots = spots;
+        int garage[][] = new int[floors][spots]; //garage array
 
     }
 
-    //method 1, 
-    public int getFloor(){
-        return floor;
+    //method to get floor, 
+    public int getFloors(){
+        return floors;
     }
 
-    //method 2
+    //method to get spots
     public int getSpots(){
         return spots;
     }
 
-    public String toString() {
-        return ("The parking floor are" + this.getFloor() + "and the parking spots available are" + this.getSpots());
-    } 
     
-}
 
-/*
 
-    public class available(int[][] spots)
 
-    public int getSpots(){
-        for (i=0; i<=4, i++){
-            for (j = 0; i <=4, i++){
-                if (spots == 0)
+    //Method to print info in specific way
+    public String toString() {
+        return ("The parking floor are" + this.getFloors() + "and the number of spots are" + this.getSpots());
+    } 
+
+    //create a code that searches for a specific spot
+    public String available(int[][] garage) {
+        //for (int i = 0; i < floor; i++){
+            //for (int j = 0; i < spots; j++){ //may not need a nested for loop could just do a boolean
+                if (garage[userFloor][userSpots] == 0){            //can also just do if(input[userFloor][userSpots] == 0)
+                    System.out.print("This spot is available");
+                }
+                else {
+                    System.out.println("This spot is not avaiable");
+                }       
+    }
+        
+
+
+    //method that gets all the spots
+    public String getAllSpots(){
+        for (int i=0; i<= floors; i++){
+            for (int j = 0; j <= spots; j++){
+                if (spots == 0) {
                     System.out.println("This spot is avaiable");
+                }
+                else {
+                    System.out.println("This is spot is not available");
+                }
             }
         }
     }
 
-*/
+}
+
