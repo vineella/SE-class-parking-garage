d77a89521cdf9ce6e9d311054afa04acee61577e
vineella <91093619+vineella@users.noreply.github.com>
2021-10-05 23:25:24 -0400

    Add files via upload

--- parkingGarage.java
+++ parkingGarage.java
@@ -4,11 +4,11 @@ class parkingGarage {
     int spots, floor;
 
     //constructor declaring class
-    public GUI(int floor, int spots){
+    public parkingGarage(int floor, int spots){
         this.floor = floor;
         this.spots = spots;
 
-    
+    }
 
     //method 1, 
     public int getFloor(){
@@ -21,13 +21,22 @@ class parkingGarage {
     }
 
     public String toString() {
-        return("The parking floor are: " + this.getFloor() + " and the 
-        parking spots avaiable are " + this.getSpots());
+        return ("The parking floor are" + this.getFloor() + "and the parking spots available are" + this.getSpots());
     } 
-
-
-
-    }
+    
 }
 
+/*
 
+    public class available(int[][] spots)
+
+    public int getSpots(){
+        for (i=0; i<=4, i++){
+            for (j = 0; i <=4, i++){
+                if (spots == 0)
+                    System.out.println("This spot is avaiable");
+            }
+        }
+    }
+
+*/
