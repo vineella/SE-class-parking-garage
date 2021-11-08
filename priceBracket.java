aeb783592c40e710cbe1c18930d565be3f2ee96d
dmelbs <91095587+dmelbs@users.noreply.github.com>
2021-11-07 22:01:20 -0500

    Add files via upload

--- priceBracket.java
+++ priceBracket.java
@@ -1,23 +1,9 @@
 package prices;
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;    
-import java.util.Scanner;
-   
 
-public class priceBracket {
+public class Program {
 	
-	public static double getPrice(int hours, boolean lostTicket, boolean isMember) {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		boolean hasLeft = false;
-		Scanner in = new Scanner(System.in);
-        System.out.print("Enter 1 when you have left: ");
-        int done = in.nextInt();
-        
-		double price = 0;
-		if(done == 1) {
-			DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-			price = dtf1 - dtf;
-		}
+	public static int getPrice(int hours, boolean lostTicket) {
+		int price = 0;
 		price = hours;
 		
 		if(hours>=12) {
@@ -28,15 +14,11 @@ public class priceBracket {
 			price = 18;
 		}
 		
-		if(isMember == true) {
-			price = price * 0.75;
-		}
-		
 		return price;
 	}
 	
 	public static void main(String[] args) {
-		System.out.print(getPrice(13, false, true));
+		System.out.print(getPrice(13, false));
 	}
 
 }
