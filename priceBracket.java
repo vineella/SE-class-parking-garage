bf62513ff3dc66f9279518dbc0948277696d6428
dmelbs <91095587+dmelbs@users.noreply.github.com>
2021-11-07 22:03:22 -0500

    Add files via upload

--- priceBracket.java
+++ priceBracket.java
@@ -1,24 +1,73 @@
 package prices;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+import java.util.Scanner;
+   
 
-public class Program {
+public class priceBracket {
+	static final double discount = 0.75;
+	static final double finalPrice = 12;
+	static final double lostPrice = 18;
 	
-	public static int getPrice(int hours, boolean lostTicket) {
-		int price = 0;
+	public static double getPrice(boolean lostTicket, boolean isMember, boolean hasInsurance) {
+		
+		double price = 0;
+		double change = 0;
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first and last name: ");
+        String name =  sc.nextLine();
+        
+        Scanner inf = new Scanner(System.in);
+        System.out.print("Enter hours parked: ");
+        double hours = inf.nextDouble();
+		
 		price = hours;
 		
 		if(hours>=12) {
-			price = 12;
+			price = finalPrice;
 		}
 		
 		if(lostTicket == true) {
-			price = 18;
+			price = lostPrice;
 		}
 		
+		if(isMember == true) {
+			price = price * discount;
+		}
+		
+		if (hasInsurance == true) {
+			price = price + 5;
+		}
+		
+		if(hasInsurance == true && lostTicket == true) {
+			price = hours;
+		}
+		
+		System.out.println(name + ", you owe $" + price + " today");
+		
+		
+		Scanner in = new Scanner(System.in);
+        System.out.print("Enter cash ammount: ");
+        double moneyGiven = in.nextInt();
+        
+        if(moneyGiven < price) {
+        	Scanner tp = new Scanner(System.in);
+        	System.out.print("Insufficient funds please insert more cash: ");
+            double money = tp.nextInt();
+            moneyGiven = moneyGiven + money;
+  
+        }
+        
+		change = moneyGiven - price;
+		System.out.println("Thank you " + name + ", your change is: $" + change);
+		System.out.println("Have a nice day!");
+	
 		return price;
 	}
 	
 	public static void main(String[] args) {
-		System.out.print(getPrice(13, false));
+		getPrice(true, false, true);
 	}
 
 }
